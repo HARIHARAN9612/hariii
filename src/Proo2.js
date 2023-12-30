af57403c7c5a84d5513e791d6ed8fd4dcf81a9be
HARIHARAN9612 <151616512+HARIHARAN9612@users.noreply.github.com>
2023-12-30 21:58:34 +0530

    Add files via upload

---
+++ src/Proo2.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './App.css';
+
+export default function Proo2() {
+  const image1 = "https://car-images.bauersecure.com/wp-images/160400/077_lamborghini_invencible.jpg";
+  const image2 = "https://car-images.bauersecure.com/wp-images/160400/1752x1168/075_lamborghini_invencible.jpg?mode=max&quality=90&scale=down";
+
+  const [image, setImage] = useState(image1);
+
+  function handleclick() {
+    setImage(image === image1 ? image2 : image1);
+  }
+  
+  return (
+    <div>
+      <h1>REACT PROJECT 2 (click the button)</h1>
+      <img src={image} alt="imagedisp" />
+      <br></br>
+      <button onClick={handleclick}>CHANGE IMAGE</button>
+    </div>
+  );
+}
